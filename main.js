94aa222df628ab7611947ab34819ddcf04abeb27
Stay_Forever <90510175+Blink-Me@users.noreply.github.com>
2023-06-19 20:14:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(500, 320);
+    canvas.center();
+}
+
+function draw() {
+
+}
+
+function take_snapshot(){
+    SVGAElement('MyFilterImage.png');
+}
